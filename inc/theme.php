cf10f1a98955217b07fb1aa833716b394117610f
Kundius Ruslan <kundius.ruslan@yandex.ru>
2026-09-13 21:22:02 +0300

    шорткод [sitemap]: карта сайта (страницы + рубрики со статьями)

--- inc/theme.php
+++ inc/theme.php
@@ -23,6 +23,55 @@ add_shortcode('partial', function ($atts, $content = null) {
   return $output;
 });
 
+add_shortcode('sitemap', function () {
+  $sitemap = '<section class="sitemap">';
+
+  $sitemap .= '<div class="sitemap__group">';
+  $sitemap .= '<h2 class="sitemap__title">Страницы</h2>';
+  $sitemap .= '<ul class="sitemap__list">';
+  $sitemap .= wp_list_pages([
+    'title_li' => '',
+    'echo' => 0,
+    'sort_column' => 'menu_order, post_title',
+  ]);
+  $sitemap .= '</ul>';
+  $sitemap .= '</div>';
+
+  $sitemap_categories = get_categories(['hide_empty' => true]);
+  if ($sitemap_categories) {
+    $sitemap .= '<div class="sitemap__group">';
+    $sitemap .= '<h2 class="sitemap__title">Рубрики</h2>';
+    $sitemap .= '<ul class="sitemap__list">';
+    foreach ($sitemap_categories as $sitemap_category) {
+      $sitemap_posts = get_posts([
+        'category' => $sitemap_category->term_id,
+        'numberposts' => -1,
+        'orderby' => 'date',
+        'order' => 'DESC',
+      ]);
+      if (empty($sitemap_posts)) {
+        continue;
+      }
+      $sitemap .= '<li>';
+      $sitemap .= '<a class="sitemap__link" href="' . esc_url(get_term_link($sitemap_category)) . '">' .
+        esc_html($sitemap_category->name) . '</a>';
+      $sitemap .= '<ul class="sitemap__sublist">';
+      foreach ($sitemap_posts as $sitemap_post) {
+        $sitemap .= '<li><a class="sitemap__link" href="' . esc_url(get_permalink($sitemap_post)) . '">' .
+          esc_html(get_the_title($sitemap_post)) . '</a></li>';
+      }
+      $sitemap .= '</ul>';
+      $sitemap .= '</li>';
+    }
+    $sitemap .= '</ul>';
+    $sitemap .= '</div>';
+  }
+
+  $sitemap .= '</section>';
+
+  return $sitemap;
+});
+
 function get_pagination($query)
 {
   $links = paginate_links([
--- main.js
+++ main.js
@@ -17,6 +17,7 @@ import './src/styles/footer.css'
 import './src/styles/breadcrumbs.css'
 import './src/styles/contacts.css'
 import './src/styles/news.css'
+import './src/styles/sitemap.css'
 import './src/styles/consult.css'
 import './src/styles/advantages.css'
 
